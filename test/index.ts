be4e9ecb13a7c2922b5a1c05f6b31602a2d23349
taishinaritomi <taishinaritomi@gmail.com>
2022-03-29 16:47:23 +0900

    Add test duplicateCheck and allAlphabet

--- test/index.ts
+++ test/index.ts
@@ -1,15 +1,35 @@
-import { base64ToEmoji, emojiToBase64 } from '../src/index';
+import { base64ToEmoji, emojiToBase64, emojiPattern } from '../src/index';
 
 const alphabet =
-  '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ/+=-_';
+  '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_/+=-';
 
-function test(str = alphabet) {
+function emojiParserCheck(str = alphabet) {
   const emoji = base64ToEmoji(str);
   const base64 = emojiToBase64(emoji);
   if (str === base64) {
-    console.log('success');
+    return 'success emojiParserCheck';
   } else {
-    console.error('error');
+    throw new Error("emojiParserCheck");
   }
 }
-test();
+
+function duplicateCheck(len = Object.values(emojiPattern)) {
+  var set = new Set(len);
+  if(set.size === len.length) {
+    return 'success duplicateCheck';
+  } else {
+    throw new Error("duplicateCheck");
+  }
+}
+
+function allAlphabet(len = Object.keys(emojiPattern)) {
+  if(len.join("") === alphabet) {
+    return 'success allAlphabets';
+  } else {
+    throw new Error("allAlphabets");
+  }
+}
+
+console.log(emojiParserCheck());
+console.log(duplicateCheck());
+console.log(allAlphabet());
